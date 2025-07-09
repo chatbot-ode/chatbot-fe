0a5d673aecc97fcc1cf8abd8c54144f8f5bd785a
Daeun-Oh <annsomde@naver.com>
2025-07-09 18:23:11 +0900

    기본 구성

---
+++ src/global/styles/color.js
@@ -0,0 +1,12 @@
+const color = {
+  primary: '#98768E',
+  secondary: '#D3B8C5',
+  success: '#82BD4D',
+  info: '#0288d1',
+  warning: '#f9a825',
+  danger: '#c62828',
+  light: '#fff',
+  dark: '#000',
+};
+
+export default color;
